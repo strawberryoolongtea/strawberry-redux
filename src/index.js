f98a37553bd29bfa5f72972adeb4655ac05bfedc
strawberryoolongtea <strawberrycream@kakao.com>
2022-02-27 19:35:26 +0900

    Vanilla Counter

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const add = document.querySelector(".add");
+const minus = document.querySelector(".minus");
+const number = document.querySelector("span");
+
+let count = 0;
+
+const updateText = () => (number.innerText = count);
+
+const handleAdd = () => {
+  count = count + 1;
+  updateText();
+};
+const handleMinus = () => {
+  count = count - 1;
+  updateText();
+};
+
+add.addEventListener("click", handleAdd);
+minus.addEventListener("click", handleMinus);
